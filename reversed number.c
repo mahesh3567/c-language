19a68317b9b39b7ebca79bd583fec30a01f2e421
mahesh3567 <128398184+mahesh3567@users.noreply.github.com>
2023-03-22 09:05:00 +0530

    Add files via upload

---
+++ reversed number.c
@@ -0,0 +1,21 @@
+//Program to find tthe reverse number
+#include <stdio.h>
+
+int main() {
+
+  int num, rev = 0, rem;
+
+  printf("Enter an number: ");
+  scanf("%d", &num);
+
+  while (num != 0) {
+    rem = num % 10;
+    rev = rev * 10 + rem;
+    num =num/10;
+  }
+
+  printf("Reversed number = %d", rev);
+
+  return 0;
+}
+
