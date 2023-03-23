dd418876a21f4354dd4477173418b7965ebced9a
mahesh3567 <128398184+mahesh3567@users.noreply.github.com>
2023-03-23 09:18:30 +0530

    Add files via upload

---
+++ day 3 class/constants,vowels,repetad letters of given word.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char sentence[100];
+    int i,j;
+    printf("Enter a sentence: ");
+    fgets(sentence, 100, stdin);
+
+    int len = strlen(sentence);
+    int word_start = 0;
+    int word_end = 0;
+
+    printf("Consonants: ");
+    for (i = 0; i < len; i++) {
+        char c = sentence[i];
+        if (c == ' ' || c == '\n' || c == '\t' || i == len - 1) {
+            word_end = i - 1;
+            if (i == len - 1) {
+                word_end = i;
+            }
+            for (j = word_start; j <= word_end; j++) {
+                char ch = sentence[j];
+                if (ch >= 'A' && ch <= 'Z') {
+                    ch += 32;
+                }
+                if (ch >= 'a' && ch <= 'z') {
+                    if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
+                        printf("%c", ch);
+                    }
+                }
+            }
+            printf(" ");
+            word_start = i + 1;
+        }
+    }
+    printf("\n");
+
+    printf("Vowels: ");
+    word_start = 0;
+    for (i = 0; i < len; i++) {
+        char c = sentence[i];
+        if (c == ' ' || c == '\n' || c == '\t' || i == len - 1) {
+            word_end = i - 1;
+            if (i == len - 1) {
+                word_end = i;
+            }
+            for (j = word_start; j <= word_end; j++) {
+                char ch = sentence[j];
+                if (ch >= 'A' && ch <= 'Z') {
+                    ch += 32; 
+                }
+                if (ch >= 'a' && ch <= 'z') {
+                    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                        printf("%c", ch);
+                    }
+                }
+            }
+            printf(" ");
+            word_start = i + 1;
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
